6eb5a4c91a7e757299e83f6a1f781c04abe089c2
onesword0618 <ao.akua.leo@gmail.com>
2019-12-02 15:03:36 +0900

    Add: bashの設定ファイルを作成する
    既にbashの設定ファイルが作成されていた場合には、処理をスキップする
    
    ここで本格的にshellのif文を作成した。
    評価式とディレクトリを調べるのがまだ雰囲気で書いているので修正していく。

---
+++ shell/init_bash.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Initialize Create .bash_profile and .bashrc.
+
+# Use Ubuntu 18.04.3 LTS
+lsb_release -d -r
+
+# Author onesword0618
+
+# Exection
+readonly bash_profile=.bash_profile
+readonly bashrc=.bashrc
+
+# 1. move home directory
+cd ~
+# 2.Create .bash_profile/.bashrc
+if [ ! -e $bash_profile -a ! -e $bashrc ]; then
+ touch .bash_profile && touch .bashrc
+fi
+# 3.Result check
+ls -a
