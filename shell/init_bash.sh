cd2dc2e8b29d517c1787d9c3c1ab0acf679e60cf
onesword0618 <ao.akua.leo@gmail.com>
2019-12-03 12:12:15 +0900

    Update: コメントの空白を修正

--- shell/init_bash.sh
+++ shell/init_bash.sh
@@ -13,9 +13,9 @@ readonly bashrc=.bashrc
 
 # 1. move home directory
 cd ~
-# 2.Create .bash_profile/.bashrc
+# 2. Create .bash_profile/.bashrc
 if [ ! -e $bash_profile -a ! -e $bashrc ]; then
  touch .bash_profile && touch .bashrc
 fi
-# 3.Result check
+# 3. Result check
 ls -a
